97c43e6367ce11a70434eca1a8b25aad11972282
Emiliano1709 <emiliano.lopez1417@gmail.com>
2025-12-03 11:46:40 -0600

    Commit de la versión final del proyecto

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # bluetab_OCR
  La siguiente prueba tiene como objetivo evaluar el manejo de herramientas OCR para la extracción de datos.
+
+## Estructura general
+Se usará streamlit como una interfaz práctica para el usuario, además del servicio de despliegue.
+La estructura general del proyecto, hasta la versión V.1.1.0 es:
+```
+prueba_OCR/
+├── datos/
+|   └── instrucciones.txt
+├── hocr/
+|   ├── 1C_5.hocr
+|   ├── 1E_1.hocr
+|   └── 3T_4.hocr
+├── recursos/
+|   ├── 1C.pdf
+|   ├── 1E.pdf
+|   ├── 3T.pdf
+|   ├── Prueba tecnica para desarrollador IA_ revisado.pdf
+|   └── notas.txt
+├── main.py
+├── requirements.txt
+└── README.md
+```
+
+### V.1.0.0
+Se hace el principio de la interfaz y se monta el servicio de despliegue
+
+### V.1.1.0
+Se hace la estructura del proyecto en subcarpetas con recursos de apoyo como las vistas previas de PDF, las instrucciones revisadas, etc., además de los archivos .hocr y algunos datos como archivos de texto grandes que se leen en la interfaz, pero no están incrustados directamente en el código.
+
+### V.1.2.2
+La interfaz despliega los PDF de referencia y una tabla con los datos extraídos de cada documento, además de que la función que encuentra las líneas, lo guarda en DataFrames de cada documento
+
+### V.1.2.?
+No se logró con exito total el usar las etiquetas de ocr_carea como haders de los DataFrames para poder separar las líneas en una estructura distinta.
+
+### V.1.3.2
+Se hizo un cambio en la función de la lectura de los dicumentos .hocr que ahora busca las etiquetas carea para formar bloques en donde introduce las etiquetas de ese bloque, procurando formar una tabla estructurada que recupere los datos y los reconstruya.
+(La tabla no reconstruye el documento, pero presenta la información con uns estructura nueva).
---
+++ main.py
@@ -0,0 +1,106 @@
+###################################################
+#                Lector de .hocr                  #
+#                                                 #
+# V.1.0.0 //02 12 2025//                          #
+# V.1.1.0 //          //                          #
+# V.1.2.2 //03 12 2025//                          #
+# V.1.3.2 //          //                          #
+# Desplegado con streamlit                        #
+# Desarrollador: Sergio Emiliano López Bautista   #
+#                                                 #
+###################################################
+
+# ------------------------- Requerimientos y librerías -------------------------------
+import os
+import codecs
+import pandas as pd
+import streamlit as st
+from bs4 import BeautifulSoup
+
+# --------------------------- Seteadores ----------------------------------------------
+st.set_page_config(page_title="Lector de HOCR",
+                   page_icon = "👀",
+                   layout="wide")
+st.title("Lector de HOCR")
+# --------------------------- Funciones -----------------------------------------------
+def instrucciones():
+    if not os.path.exists("datos/instrucciones.txt"):
+        raise FileNotFoundError(f"El archivo {"datos/instrucciones.txt"} no existe.")
+
+    with codecs.open("datos/instrucciones.txt", "r", encoding="utf-8") as f:
+        fi = f.read()
+    file = fi.split('\n')
+
+    for linea in file:
+        st.markdown(linea)
+
+
+def hocr_csv_estructurado(ruta_hocr):
+    
+    # Leemos el archivo HOCR
+    with open(ruta_hocr, "r", encoding="utf-8") as f:
+        sopa = BeautifulSoup(f, "html.parser")
+    
+    # Extraemos la info de cada palabra
+    data = []
+
+    ## Primera iteración para el carea
+    # Por cada elemento carea hacemos un bloque
+    for a in sopa.find_all("span", class_="ocr_carea"):
+
+        # Dentro de cada bloque encontramos las líneas de texto
+        lineas = a.find_all("span", class_="ocr_line")  
+        
+        #La Línea 0 es el header
+        header = lineas[0].get_text(strip=True)
+        ## Segunda iteración para las líneas dentro del bloque carea
+        # Por cada línea, cachamos el texto
+        for b in lineas[1:]:
+            texto = b.get_text(strip=True)
+            data.append({header: texto})
+    return data
+
+# -------------------------------- Interfaz (MAIN)-------------------------------------
+#Ponemos las instrucciones
+instrucciones()
+
+# Momentaneamente no usamos bucles para poder ver claramente cada uno de los documentos
+
+#ejemplo 1
+frame1 = pd.DataFrame(hocr_csv_estructurado("hocr/1C_5.hocr"))
+
+st.markdown("# Ejemplo 1")
+st.pdf("recursos/1C.PDF")
+st.dataframe(frame1)
+st.download_button(
+    label = "1C - Descargar CSV",
+    data = frame1.to_csv(index=False).encode('utf-8'),
+    file_name = "1C_csv.csv",
+    mime = "text/csv",
+)
+
+#ejemplo 2
+frame2 = pd.DataFrame(hocr_csv_estructurado("hocr/1E_1.hocr"))
+
+st.markdown("# Ejemplo 2")
+st.pdf("recursos/1E.pdf")
+st.dataframe(frame2)
+st.download_button(
+    label = "1E - Descargar CSV",
+    data = frame2.to_csv(index=False).encode('utf-8'),
+    file_name = "1E_csv.csv",
+    mime = "text/csv",
+)
+
+#ejemplo 3
+frame3 = pd.DataFrame(hocr_csv_estructurado("hocr/3T_4.hocr"))
+
+st.markdown("# Ejemplo 3")
+st.pdf("recursos/3T.pdf")
+st.dataframe(frame3)
+st.download_button(
+    label = "3T - Descargar CSV",
+    data = frame3.to_csv(index=False).encode('utf-8'),
+    file_name = "3T_csv.csv",
+    mime = "text/csv",
+)
